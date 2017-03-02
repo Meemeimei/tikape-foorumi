fb59e0c9b3d75cdc8f61b55ca5fee0348d5c392a
Joonas Saha <joonassaha@Joonass-MacBook-Pro.local>
2017-03-02 23:01:04 +0200

    database-luokka tehty

--- src/main/java/tikape/foorumirunko/database/KayttajaDao.java
+++ src/main/java/tikape/foorumirunko/database/KayttajaDao.java
@@ -1,17 +1,14 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package tikape.foorumirunko.database;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
+import tikape.foorumirunko.domain.Kayttaja;
 
-/**
- *
- * @author eemitant
- */
 public class KayttajaDao implements Dao {
 
     @Override
